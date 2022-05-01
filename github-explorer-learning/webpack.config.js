01e2ec5f6c8cd4b1b91d6b85d7a276c2632ab8f1
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-01 18:29:18 -0400

    Utilizando o SASS com webpack e sass-loader

--- github-explorer-learning/webpack.config.js
+++ github-explorer-learning/webpack.config.js
@@ -32,9 +32,8 @@ module.exports = {
                 use: 'babel-loader',
             }, 
             {
-                test: /\.css$/,
-                //exclude: /node_modules/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     }
