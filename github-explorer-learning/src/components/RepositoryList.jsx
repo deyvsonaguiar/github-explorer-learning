18f31ed6881d72131e78326e59b45635340e2f5e
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-16 18:03:01 -0400

    Estilizando a listagem

--- github-explorer-learning/src/components/RepositoryList.jsx
+++ github-explorer-learning/src/components/RepositoryList.jsx
@@ -13,6 +13,8 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
             <ul>
                 <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
